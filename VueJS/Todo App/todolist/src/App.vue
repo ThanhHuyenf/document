384f4100232352426141bdf7206d8dd9b0de626e
ThanhHuyenf <huyenmotnang@gmail.com>
2021-08-07 00:46:51 +0700

    todolist

---
+++ VueJS/Todo App/todolist/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="container">
+    <Todolist></Todolist>
+  </div>
+</template>
+
+<script>
+import Todolist from './components/TodoList'
+export default {
+  name: 'App',
+  components: {Todolist}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 24px;
+}
+.container{
+  width: 400px;
+  margin: 0 auto;
+  box-sizing: border-box;
+}
+</style>
---
+++ VueJS/Todo App/todolist/src/components/TodoList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What need to be done?"
+           v-model="newTodo"
+           @keyup.enter="addTodo">
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div>{{ todo.title }}</div>
+      <div class="remove-item" @click="removeTodo(index)">&times;</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data () {
+    return {
+      idForTodo: 3,
+      newTodo: '',
+      todos: [{
+        'id': 1,
+        'title': 'huyefn',
+        'complete': false
+      }, {
+        'id': 2,
+        'title': 'thanhhuyenf',
+        'complete': false
+      }
+      ]
+    }
+  },
+  methods: {
+    addTodo () {
+      if (this.newTodo.trim() === 0) {
+        return
+      }
+      this.todos.unshift({
+        id: this.idForTodo,
+        title: this.newTodo,
+        complete: false
+      })
+      this.newTodo = ''
+      this.idForTodo++
+    },
+    removeTodo (index) {
+      this.todos.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+*{
+  box-sizing: border-box;
+}
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+}
+.todo-item{
+  display: flex;
+  align-items: center;
+  margin-top: 12px;
+  justify-content: space-between;
+}
+.remove-item{
+  cursor: pointer;
+}
+</style>
